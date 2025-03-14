49524ef9b74bad60deacf661151e676217bc3590
lskq <lskq012025@gmail.com>
2025-03-14 09:46:53 +0100

    Attempt async console music and animation

--- Program.cs
+++ Program.cs
@@ -1,36 +1,83 @@
-﻿using System.Dynamic;
+﻿using System.Threading.Tasks;
 
 namespace JukeboxProofOfConcept;
 
 
 public class JukeboxProofOfConcept
 {
-    static void Main(string[] args)
+    static bool Async = true;
+
+    static async Task Main(string[] args)
     {
         Melody melody = Melody.GetTetrisA(140);
+        bool verbose = false;
 
-        Play(melody, true);
+        if (args.Length > 0 && args.Contains("-v") || args.Contains("-verbose"))
+        {
+            verbose = true;
+        }
+        if (args.Length > 0 && args.Contains("-a") || args.Contains("-async"))
+        {
+            bool done = false;
+
+            done = await AsyncPlay(melody, verbose);
+
+            var coords = Console.GetCursorPosition();
+
+            if (!verbose)
+                do { Anim(coords.Left, coords.Top); } while (!done);
+        }
+        else
+        {
+            _ = Play(melody, true);
+        }
     }
 
-    static void Play(Melody melody, bool verbose = false)
+    static void Anim(int left, int top)
     {
-        var tune = melody.Tune;
-        int mpsb = melody.Mpsb;
+        char[] frames = ['/', '—', '\\', '|'];
 
-        foreach (var tone in tune)
+        foreach (char frame in frames)
         {
-            int freq = (int)tone.Item1;
-            freq = freq >= 37 ? freq : 0;
-            int time = (int)((int)tone.Item2 * mpsb * 1.0);
+            Console.SetCursorPosition(left, top);
+            Console.Write(frame);
+            Thread.Sleep(500);
+        }
+    }
 
-            if (verbose) Console.WriteLine($"Hz:{freq}, Ms: {time}");
+    static async Task<bool> AsyncPlay(Melody melody, bool verbose = false)
+    {
+        return await Task.Run(() => Play(melody, verbose));
+    }
 
-            if (freq != 0)
+    static bool Play(Melody melody, bool verbose = false)
+    {
+        try
+        {
+            var tune = melody.Tune;
+            int mpsb = melody.Mpsb;
+
+            foreach (var tone in tune)
             {
-                Console.Beep(freq, time);
+                int freq = (int)tone.Item1;
+                freq = freq >= 37 ? freq : 0;
+                int time = (int)((int)tone.Item2 * mpsb * 1.0);
+
+                if (verbose) Console.WriteLine($"Hz:{freq}, Ms: {time}");
+
+                if (freq != 0)
+                {
+                    Console.Beep(freq, time);
+                }
+                else
+                    Thread.Sleep(time);
             }
-            else
-                Thread.Sleep(time);
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
         }
     }
 }
