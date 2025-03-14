5df4d9679aff80f7d7f730cd420458db5e5fc528
lskq <lskq012025@gmail.com>
2025-03-14 10:47:29 +0100

    Implement async music and animation

--- Program.cs
+++ Program.cs
@@ -8,49 +8,40 @@ public class JukeboxProofOfConcept
     static async Task Main(string[] args)
     {
         Melody melody = Melody.GetTetrisA(140);
-        bool verbose = false;
 
         if (args.Length > 0 && args.Contains("-v") || args.Contains("-verbose"))
         {
-            verbose = true;
-        }
-        if (args.Length > 0 && args.Contains("-a") || args.Contains("-async"))
-        {
-            bool done = false;
-
-            done = await AsyncPlay(melody, verbose);
-
-            var coords = Console.GetCursorPosition();
-
-            if (!verbose)
-                do { Anim(coords.Left, coords.Top); } while (!done);
+            _ = Play(melody, true);
         }
         else
         {
-            _ = Play(melody, verbose);
+            var (left, top) = Console.GetCursorPosition();
+
+            //await Anim(left, top);
+            //await Play(melody, false);
+
+            var animTask = Anim(left, top);
+            var playTask = Play(melody, false);
+
+            await animTask;
+            await playTask;
         }
-    }
 
-    static void Anim(int left, int top)
-    {
-        char[] frames = ['/', '—', '\\', '|'];
-
-        foreach (char frame in frames)
+        static async Task Anim(int left, int top)
         {
-            Console.SetCursorPosition(left, top);
-            Console.Write(frame);
-            Thread.Sleep(500);
+            char[] frames = ['/', '—', '\\', '|'];
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (char frame in frames)
+                {
+                    Console.SetCursorPosition(left, top);
+                    Console.Write(frame);
+                    await Task.Delay(500);
+                }
+            }
         }
-    }
 
-    static async Task<bool> AsyncPlay(Melody melody, bool verbose = false)
-    {
-        return await Task.Run(() => Play(melody, verbose));
-    }
-
-    static bool Play(Melody melody, bool verbose = false)
-    {
-        try
+        static async Task Play(Melody melody, bool verbose = false)
         {
             var tune = melody.Tune;
             int mpsb = melody.Mpsb;
@@ -68,14 +59,8 @@ public class JukeboxProofOfConcept
                     Console.Beep(freq, time);
                 }
                 else
-                    Thread.Sleep(time);
+                    await Task.Delay(time);
             }
-
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
         }
     }
 }
