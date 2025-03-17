d380304a3e11e02901d1cb138231ba401f373d21
lskq <lskq012025@gmail.com>
2025-03-17 13:22:25 +0100

    Remove tat

--- Program.cs
+++ Program.cs
@@ -6,62 +6,51 @@ public class JukeboxProofOfConcept
     {
         Melody melody = Melody.GetTetrisA(140);
 
-        if (args.Length > 0 && "animation".StartsWith(args[0][1..]))
-        {
-            var (left, top) = Console.GetCursorPosition();
+        var (left, top) = Console.GetCursorPosition();
 
-            //await Anim(left, top);
-            //await Play(melody, false);
+        //await Anim(left, top);
+        //await Play(melody, false);
 
-            var animTask = Anim(left, top, 4);
-            var playTask = Play(melody, false);
+        var animTask = Anim(left, top, 4);
+        var playTask = Play(melody, false);
 
-            await animTask;
-            await playTask;
-        }
-        else if (args.Length > 0 && "verbose".StartsWith(args[0][1..]))
-        {
-            _ = Play(melody, true);
-        }
-        else
-        {
-            _ = Play(melody, false);
-        }
+        await animTask;
+        await playTask;
+    }
 
-        static async Task Anim(int left, int top, int loops = 1)
+    static async Task Anim(int left, int top, int loops = 1)
+    {
+        char[] frames = ['/', '—', '\\', '|'];
+        for (int i = 0; i < loops; i++)
         {
-            char[] frames = ['/', '—', '\\', '|'];
-            for (int i = 0; i < loops; i++)
+            foreach (char frame in frames)
             {
-                foreach (char frame in frames)
-                {
-                    Console.SetCursorPosition(left, top);
-                    Console.Write(frame);
-                    await Task.Delay(500);
-                }
+                Console.SetCursorPosition(left, top);
+                Console.Write(frame);
+                await Task.Delay(500);
             }
         }
+    }
 
-        static async Task Play(Melody melody, bool verbose = false)
+    static async Task Play(Melody melody, bool verbose = false)
+    {
+        var tune = melody.Tune;
+        int mpsb = melody.Mpsb;
+
+        foreach (var tone in tune)
         {
-            var tune = melody.Tune;
-            int mpsb = melody.Mpsb;
+            int freq = (int)tone.Item1;
+            freq = freq >= 37 ? freq : 0;
+            int time = (int)((int)tone.Item2 * mpsb * 1.0);
 
-            foreach (var tone in tune)
+            if (verbose) Console.WriteLine($"Hz:{freq}, Ms: {time}");
+
+            if (freq != 0)
             {
-                int freq = (int)tone.Item1;
-                freq = freq >= 37 ? freq : 0;
-                int time = (int)((int)tone.Item2 * mpsb * 1.0);
-
-                if (verbose) Console.WriteLine($"Hz:{freq}, Ms: {time}");
-
-                if (freq != 0)
-                {
-                    Console.Beep(freq, time);
-                }
-                else
-                    await Task.Delay(time);
+                Console.Beep(freq, time);
             }
+            else
+                await Task.Delay(time);
         }
     }
 }
